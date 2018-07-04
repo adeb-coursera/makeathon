fa6fd6af2cace991b5d1356bcf14b7d85bb46188
Akashdeep Deb <deb2@illinois.edu>
2018-07-04 01:36:05 -0700

    added logo

--- lunchtime/allofcoursera.py
+++ lunchtime/allofcoursera.py
@@ -14,20 +14,21 @@ def main():
 	name_to_mail = dict()
 	pp = pprint.PrettyPrinter(indent=4)
 
-	with codecs.open('allcoursera.csv', encoding="utf-8-sig") as csvfile:
+	with codecs.open('allcoursera.csv') as csvfile:
 		rowreader = csv.DictReader(csvfile)
-		# first = next(rowreader)
-		# print(first)
-		for r in rowreader:
-			print(r)
-			# row = r.encode("utf-8")
-			# mail_to_info[row['mail']] = (row['name'], row['department'], row['title'], row['st'], row['co'], row['manager'], row['directReports'])
-			# mail_to_name[row['mail']] = row['name']
-			# name_to_mail[row['name']] = row['mail']
+		for row in rowreader:
+			name = '\xef\xbb\xbfname'
+			mail_to_info[row['mail']] = (row[name], row['department'], row['title'], row['st'], row['co'], row['manager'], row['directReports'])
+			mail_to_name[row['mail']] = row[name]
+			name_to_mail[row[name]] = row['mail']
 
 	with open('employees.json', 'w') as file:
 		json.dump(mail_to_info, file)
 
-	pp.pprint(mail_to_info)
+	with open('emp_mail_to_name.json', 'w') as file:
+		json.dump(mail_to_name, file)
 
+	with open('emp_name_to_mail.json', 'w') as file:
+		json.dump(name_to_mail, file)
+		
 main()
